a412c740e10f4de1eb19b3bb968d7b83b5bae016
DavideDello <dellomitico98@gmail.com>
2026-04-08 21:21:52 +0200

    fix

--- src/helpers/monsterDataFromStatblockName.ts
+++ src/helpers/monsterDataFromStatblockName.ts
@@ -1,10 +1,18 @@
 import monsterIndex from "../statblockSearch/monsterIndex.json";
 import { getMonsterDataBundle } from "../statblockSearch/helpers/getMonsterDataBundle.tsx";
+import type { IndexBundle } from "../types/monsterDataBundlesZod";
 
 export const monsterDataFromStatblockName = async (monsterId: string) => {
   const indexBundle = monsterIndex.find((val) => val.name === monsterId);
   if (indexBundle === undefined) {
     throw new Error("Could not find statblock with name " + monsterId);
   }
-  return await getMonsterDataBundle(indexBundle);
+  const normalizedIndexBundle: IndexBundle = {
+    ...indexBundle,
+    skills:
+      "skills" in indexBundle && Array.isArray(indexBundle.skills)
+        ? indexBundle.skills
+        : [],
+  };
+  return await getMonsterDataBundle(normalizedIndexBundle);
 };
